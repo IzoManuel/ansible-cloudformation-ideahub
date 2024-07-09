bb390110c8a442c064c7eb91a17d16a39e47d051
IzoManuel <israel7manuel@gmail.com>
2024-07-09 15:58:37 +0300

    Configure ECS

--- cloudformation-ideahub/scripts/deploy.py
+++ cloudformation-ideahub/scripts/deploy.py
@@ -4,6 +4,7 @@ import os
 from botocore.exceptions import ClientError
 import subprocess
 
+
 def deploy_stack(stack_name, template_file):
     with open(template_file, 'r') as template:
         template_body = template.read()
@@ -61,38 +62,43 @@ def deploy_stack(stack_name, template_file):
             StackName=stack_name,
             TemplateBody=template_body,
             Parameters=parameters,
-            Capabilities=['CAPABILITY_NAMED_IAM','CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND']
+            Capabilities=['CAPABILITY_NAMED_IAM',
+                          'CAPABILITY_NAMED_IAM', 'CAPABILITY_AUTO_EXPAND']
         )
         print(f"Creating stack {stack_name} initiated.")
 
     # Wait for the stack to be created/updated
-    waiter = cloudformation.get_waiter('stack_create_complete' if not stack_exists else 'stack_update_complete')
+    waiter = cloudformation.get_waiter(
+        'stack_create_complete' if not stack_exists else 'stack_update_complete')
     waiter.wait(StackName=stack_name)
     print(f"Stack {stack_name} deployment complete")
 
     return response
 
+
 def get_stack_output(stack_name):
     cloudformation = boto3.client('cloudformation')
     response = cloudformation.describe_stacks(StackName=stack_name)
-    stack =response['Stacks'][0]
+    stack = response['Stacks'][0]
 
     outputs = {}
     for output in stack['Outputs']:
         outputs[output['OutputKey']] = output['OutputValue']
     return outputs
 
+
 def update_inventory(ip_address):
     inventory_content = f"[mysql_servers]\n{ip_address}\n"
     with open('../../ansible-ideahub/inventory/hosts.ini', 'w') as inventory_file:
         inventory_file.write(inventory_content)
 
+
 if __name__ == "__main__":
     # Adjust paths to be relative to the script location
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    template_file = os.path.join(script_dir, '../templates/ec2-instance.yml')
+    template_file = os.path.join(script_dir, '../templates/ideahub-api.yml')
 
-    stack_name = "ideahub-ec2-stack"
+    stack_name = "ideahub-api-stack"
     deploy_stack(stack_name, template_file)
 
     # Get stack outputs
@@ -101,4 +107,3 @@ if __name__ == "__main__":
     update_inventory(public_ip)
 
     # os.system("ansible-playbook -i ../../ansible-ideahub/inventory/hosts.ini ../../ansible-ideahub/playbook.yml --ask-vault-pass")
-
